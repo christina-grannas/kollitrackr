86bd541d33be56c2997c11787c761713c722fb55
rubbad <christina.grannas@gmail.com>
2008-09-26 13:51:55 +0200

    Adding tracking history and nice formatting.

--- kollitrackr.sh
+++ kollitrackr.sh
@@ -1,18 +1,52 @@
 #!/bin/bash
+# Söka kolli på posten.se
 
-if [ $1 ]; then
 
-number=$1
+kolliid=$1
+programnamn=$0
 
-else
 
-echo -n "Ange ditt kollinummer:"
-read number
+#Kontrollera att kolli-id är angett:
+
+if [ ! $kolliid ]
+	then
+		echo "Ange kolli-id som argument!"
+		echo "Exempel:" $programnamn "[kolli-id]"
+		exit
+		echo "Om denna text syns, så är det fel nånstans."
+
+	else
+		echo "Spårar kolli" $1 "via posten.se..."
 fi
 
-wget -O response.html "http://www.posten.se/tracktrace/TrackConsignments_do.jsp?trackntraceAction=saveSearch&logisticCustomerNumber=&referenceNumber=&lang=SE&loginHandlerImplClass=se.posten.pse.framework.security.applicationImpl.tracktrace.LoginHandlerImpl&internalPageNumber=0&doNotShowInHistory=true&consignmentId=$number&consignmentId=&consignmentId=&consignmentId=&consignmentId=&consignmentId=&consignmentId=&consignmentId="
 
+# Hämta (temporär) html-fil från posten.se, $kolliid angett i url:
+wget -qO $1.html "http://www.posten.se/tracktrace/TrackConsignments_do.jsp?trackntraceAction=saveSearch&logisticCustomerNumber=&referenceNumber=&lang=SE&loginHandlerImplClass=se.posten.pse.framework.security.applicationImpl.tracktrace.LoginHandlerImpl&internalPageNumber=0&doNotShowInHistory=true&consignmentId=$kolliid&consignmentId=&consignmentId=&consignmentId=&consignmentId=&consignmentId=&consignmentId=&consignmentId="
 
-grep '<h3>' ./response.html
+# Spara info om kollit i $1.kolli:
+# rm $1.kolli
+echo "ID:  " $1 > $1.kolli
+echo -ne "Från: " >> $1.kolli
+grep 'Fr&aring;n:' ./$1.html | sed 's/\///g' | sed 's/<[^>]*>//g' | sed 's/Fr&aring;n://g' | sed 's/^ *//g' | sed '/^$/d' >> $1.kolli 
+echo -ne "Till: " >> $1.kolli
+grep 'Till:' ./$1.html | sed 's/\///g' | sed 's/<[^>]*>//g' | sed 's/Till://g' | sed 's/&nbsp;//g' | sed 's/^ *//g' | sed '/^$/d' >> $1.kolli
+echo -ne "Vikt: " >> $1.kolli
+grep 'Vikt:' ./$1.html | sed 's/\///g' | sed 's/<[^>]*>//g' | sed 's/Vikt://g' | sed 's/^ *//g' | sed '/^$/d' >> $1.kolli
+echo -ne "Typ:  " >> $1.kolli
+grep 'Typ av f&ouml;rs&auml;ndelse:' ./$1.html | sed 's/\///g' | sed 's/<[^>]*>//g' | sed 's/Typ av f&ouml;rs&auml;ndelse://g' | sed 's/^ *//g' | sed '/^$/d' >> $1.kolli
+echo  >> $1.kolli
 
-rm ./response.html
+echo "Tidpunkt          Händelse" >> $1.kolli
+
+# Sortera ut relevant textoch spara i $1.kolli:
+grep 'Historik' ./$1.html | sed 's/<tr>/\
+/g' |  sed 's/<\/td>/   /g' | sed 's/<[^>]*>//g' | sed 's/Historik://g' | sed 's/DatumMeddelande//g' | sed 's/[,]//g' | sed '/^$/d' >> $1.kolli
+
+# Visa filen $1.kolli
+echo
+cat $1.kolli
+echo
+
+# Tag bort den temporära html-filen som hämtats från posten.se:
+rm ./$1.html
+
